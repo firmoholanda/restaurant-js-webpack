1de0b249a4ec9bfe718f38fde84d5e78b34365f7
firmo holanda <firmo@viize.com>
2020-05-13 16:24:22 -0300

    create navbar

--- src/navbar.js
+++ src/navbar.js
@@ -1,35 +1,23 @@
 const navbar = document.createElement('nav');
+
 const ul = document.createElement('ul');
+ul.className = 'nav nav-tabs';
 
-const home = document.createElement('li');
-const menu = document.createElement('li');
-const contact = document.createElement('li');
-const about = document.createElement('li');
+function createMenuItem(menuItems) {
+  menuItems.forEach((menuItem) => {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    a.id = menuItem;
+    a.appendChild(document.createTextNode(menuItem));
+    a.className = 'nav-link';
+    li.appendChild(a);
+    li.className = 'nav-item';
+    ul.appendChild(li);
+  });
+}
 
-//li.setAttribute('class', 'nav-item');
+createMenuItem(['home', 'menu', 'contact', 'about']);
 
-home.innerHTML = 'home';
-//home.setAttribute('class', 'nav-item');
-
-menu.innerHTML = 'menu';
-//menu.setAttribute('class', 'nav-item');
-
-contact.innerHTML = 'contact';
-//contact.setAttribute('class', 'nav-item');
-
-about.innerHTML = 'about';
-//about.setAttribute('class', 'nav-item');
-
-ul.setAttribute('class', 'nav nav-tabs');
-ul.appendChild(home);
-ul.appendChild(menu);
-ul.appendChild(contact);
-ul.appendChild(about);
-
-//navbar.setAttribute('class', 'nav navbar');
 navbar.appendChild(ul);
-
-li.setAttribute('class', 'nav-item');
-console.log(li)
 
 export default navbar;
